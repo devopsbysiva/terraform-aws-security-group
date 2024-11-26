27b4e3efb0092f46b021cbe688ad634760295caa
ganeshmoka <ganeshmoka1999@gmail.com>
2024-11-26 22:52:11 +0530

    vpc

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "aws_security_group" "main" {
   name        = local.sg_final_name
   vpc_id      = var.vpc_id
 
-  tags = merge(var.common_tags ,var.security_group_tags, 
+  tags = merge(var.common_tags ,var.security_group_tags,
+   
     {
     Name = local.sg_final_name
     }
